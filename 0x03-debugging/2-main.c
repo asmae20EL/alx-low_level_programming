a11f750185cb472f3935a4a0d321f04925f0b322
Asmae <asmae94jil@gmail.com>
2023-04-27 08:13:19 -0700

    Task 2

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+* int largest_number (int a, int b, int c)
+*/
+
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	int a = 972;
+	int b = -98;
+	int c = 0;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0)
+}
